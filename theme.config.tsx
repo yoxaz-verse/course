89bed388ba5289b18d79e5a220037417afb634c9
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-10-23 20:08:20 +0530

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>OBAOL Supreme</span>,
+  // project: {
+  //   link: "https://www.instagram.com/obaol_supreme/",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://www.obaol.com",
+  footer: {
+    text: "Obaol Sales Guide",
+  },
+};
+
+export default config;
